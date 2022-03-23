6c380333df74a47c83025653a8b24108a258dc13
Niv Gal Waizer <nwaizer@redhat.com>
2022-03-23 10:08:56 +0200

    typo

--- utils/utils.go
+++ utils/utils.go
@@ -12,7 +12,7 @@ var Namespace = "default"
 
 func GetPods() *v1.PodList {
 	Pods, err := client.Client.Pods(Namespace).List(context.Background(), v12.ListOptions{
-		LabelSelector: "app: logsdemo"})
+		LabelSelector: "app=logsdemo"})
 	if err != nil {
 		logrus.Errorf("Failed to get demo pod from cluster %v", client.Client.Config.Host)
 	}
